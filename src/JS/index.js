7a63d1d2ffa4f67daf9e8f4671b4fb1c4ccf2b56
Stanislav <stanislav_2010@icloud.com>
2023-06-26 18:15:50 +0500

    add offer, header, form stop

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,17 +1,61 @@
+function animate () {
+    const animateEl = document.querySelectorAll("[data-animate]");
+    
+    animateEl.forEach(el => {
+        if (scrollY => el.clientTop) {
+            console.log("ok");
+        }
+    })
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     const burger = document.querySelector(".header__burger");
     const menu = document.querySelector(".header__col--menu");
     const header = document.querySelector("header");
+    const errorMap = ["Пожалуйста, заполните все обязательные поля", "Укажите, пожалуйста, имя", "Слишком короткое значение"];
 
-    menu.style.top = header.clientHeight + 10 +  "px";
+    menu.style.top = header.clientHeight - 1 +  "px";
+    document.querySelector("main").style.marginTop = header.clientHeight + 1 + "px";
 
     burger.addEventListener("click", () => {
         burger.classList.toggle("active");
         menu.classList.toggle("active");
         document.body.classList.toggle("lock");
+        
     });
 
-    if (window.innerWidth <= 576) {
-        
+    window.addEventListener("scroll", () => {
+        animate();
+    });
+
+    if (document.querySelector(".form")) {
+        const forms = document.querySelectorAll(".form");
+        forms.forEach(el => {
+            let error = false;
+            const inputName = el.querySelector("input[name='name']");
+            const inputPhone = el.querySelector("input[name='phone']");
+            const label = el.querySelector(".form__label");
+            el.addEventListener("submit", (e) => {
+                e.preventDefault();
+                    if (inputName.value.length < 1 || inputPhone.value.length < 1 ) {
+                        label.classList.add("error");
+                        label.textContent = errorMap[0];
+                        console.log(inputName.value.length);
+                    } else if (inputName.value.length < 5)  {
+                        label.classList.add("error");
+                        label.textContent = errorMap[2];
+                        error = true;
+                    } else if (el.querySelector("input[name='name']").value.search(/[a-zA-Z]+[А-яЁё]/)) {
+                        label.classList.add("error");
+                        label.textContent = errorMap[1];
+                        error = true;
+                    } else {
+                        label.classList.remove("error");
+                        label.textContent = "Спасибо! Мы свяжемся с вами в ближайшее время"
+                        label.classList.add("right");
+                    }
+            })
+        })
     }
+
 });
